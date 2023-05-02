2e72718ad92caceeca4857defc61536074587c72
blake315 <1455975369@qq.com>
2023-05-02 23:10:28 +0800

    前端页面完成角色管理模块

---
+++ src/api/system/sysRole.js
@@ -0,0 +1,63 @@
+import request from '@/utils/request'
+
+const api_name = '/admin/system/sysRole'
+export default {
+    getPageList(current, limit, searchObj) {
+        return request({
+            url: `${api_name}/${current}/${limit}`,
+            method: 'get',
+            params: searchObj
+          })
+    },
+    /**
+     * 角色删除
+     * @param {*} id 
+     * @returns 
+     */
+    removeById(id) {
+        return request({
+        url: `${api_name}/remove/${id}`,
+        method: 'delete'
+        })
+    },
+
+    /**
+     * 角色添加
+     * @param {*} role 
+     * @returns 
+     */
+    save(role) {
+        return request({
+        url: `${api_name}/save`,
+        method: 'post',
+        data: role
+        })
+    },
+
+    // 回显要修改的id信息
+    getById(id) {
+        return request({
+        url: `${api_name}/get/${id}`,
+        method: 'get'
+        })
+    },
+    
+    // 修改
+    updateById(role) {
+        return request({
+        url: `${api_name}/update`,
+        method: 'put',
+        data: role
+        })
+    },
+
+    // 批量删除
+    batchRemove(idList) {
+        return request({
+        url: `${api_name}/batchRemove`,
+        method: `delete`,
+        data: idList
+        })
+    }
+    
+}
